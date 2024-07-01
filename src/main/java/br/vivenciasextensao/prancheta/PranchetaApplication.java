a75f948ed392a91739c744155e5c41a6d02fdb98
Lucas Mello <www.lucasmello@gmail.com>
2024-07-01 14:41:39 -0300

    Atualização

---
+++ src/main/java/br/vivenciasextensao/prancheta/PranchetaApplication.java
@@ -0,0 +1,13 @@
+package br.vivenciasextensao.prancheta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PranchetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PranchetaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/vivenciasextensao/prancheta/PranchetaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.vivenciasextensao.prancheta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PranchetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
