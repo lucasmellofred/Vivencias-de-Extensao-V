98278a9458d639081ce6321c9fa5326668c2f65a
Lucas Mello <lucasmello@univille.com>
2024-07-03 20:18:00 -0300

    update

--- src/main/java/br/vivenciasextensao/prancheta/controller/ItemController.java
+++ src/main/java/br/vivenciasextensao/prancheta/controller/ItemController.java
@@ -4,19 +4,34 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import br.vivenciasextensao.prancheta.entity.AcaoSocial;
 import br.vivenciasextensao.prancheta.entity.Item;
+import br.vivenciasextensao.prancheta.repository.AcaoSocialRepository;
 import br.vivenciasextensao.prancheta.repository.ItemRepository;
+import jakarta.transaction.Transactional;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
 
 
 @Controller
+@RequestMapping("/acaoSocial/{acaoSocialId}/itens")
 public class ItemController {
     @Autowired
     private ItemRepository itemRepository;
+
+    @Autowired
+    private AcaoSocialRepository acaoSocialRepository;
 
     @GetMapping("/todos-itens")
     public String item(Model model){
@@ -65,4 +80,40 @@ public class ItemController {
         itemRepository.delete(item);
         return "redirect:/todos-itens";
     }
+
+    @GetMapping("/cadastrar-item-acao-social/")
+    public String cadastrarItemAcaoSocial(Model model, @PathVariable("acaoSocialId") Long acaoSocialId) {
+        AcaoSocial acaoSocial = acaoSocialRepository.findById(acaoSocialId).get();
+        model.addAttribute("acaoSocial", acaoSocial);
+        return "gestaoDoacao/html/cadastroAcao";
+    }
+
+  
+
+    @Transactional
+    @PostMapping("/save")
+    public String saveItems(@ModelAttribute("acaoSocialId") Long acaoSocialId, @RequestParam(name = "nome", required = true) String nome, @RequestParam(name = "descricao", required = true) String descricao, @RequestParam(name = "codigoIdentificacao", required = true) String codigoIdentificacao, @RequestParam(name = "infoAdicional", required = true) String infoAdicional, @RequestParam(name = "categoria", required = true) String categoria, RedirectAttributes redirectAttributes) {
+        // Primeiro, buscar a AcaoSocial pelo ID
+        Optional<AcaoSocial> acaoSocialOptional = acaoSocialRepository.findById(acaoSocialId);
+        
+        if (acaoSocialOptional.isPresent()) {
+            AcaoSocial acaoSocial = acaoSocialOptional.get();
+            
+            // Agora que temos o objeto AcaoSocial, podemos associá-lo ao item
+            Item item = new Item();
+            item.setNome(nome);
+            item.setDescricao(descricao);
+            item.setCodigoIdentificacao(codigoIdentificacao);
+            item.setInfoAdicional(infoAdicional);
+            item.setCategoria(categoria);
+            item.setAcaoSocial(acaoSocial);
+            itemRepository.save(item);
+            
+        } else {
+            redirectAttributes.addFlashAttribute("message", "Ação social não encontrada");
+        }
+
+        return "redirect:/editar-acao-social/" + acaoSocialId;
+    }
+
 }
